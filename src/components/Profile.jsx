eabe9fd045e1a808709afffc6bd6374b08ccad72
Sathya Narayan <snp@Sathyas-MacBook-Air.local>
2023-09-07 23:05:13 +0530

    put the image inside public

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,7 +3,7 @@ import "/src/styles/Profile.css"
 export default function Profile() {
     return(
         <div className="profile-pic">
-            <img src="/images/profile.jpg" alt="" srcset="" />
+            <img src="public/profile.jpg" alt="" srcset="" />
         </div>
     )
 }
