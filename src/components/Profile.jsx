52dbf0749d9eaeee93c1bcf5b075c46c4b9b3c0a
Sathya Narayan <snp@Sathyas-MacBook-Air.local>
2023-09-07 22:55:00 +0530

    Edited img

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,7 +3,7 @@ import "/src/styles/Profile.css"
 export default function Profile() {
     return(
         <div className="profile-pic">
-            <img src="/src/assets/profile.jpg" alt="" srcset="" />
+            <img src="/images/profile.jpg" alt="" srcset="" />
         </div>
     )
 }
